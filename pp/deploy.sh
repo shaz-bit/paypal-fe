f40852db663dc6cd38ffac17954a19144de0405b
Cloud User <shaz10th@gmail.com>
2025-02-23 04:12:12 +0000

    deployment  feature modified

--- pp/deploy.sh
+++ pp/deploy.sh
@@ -2,5 +2,6 @@ Developers at work
 Application is ready to be deployed to the development environment.
 
 Developmet must  reviewed by manager
-AWS CODE DEPLOY IS A GREAT TOOL
-s
+AWS CODE DEPLOY IS A GREAT TOOLs
+Deployment with ancible
+
